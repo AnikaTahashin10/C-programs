d018dc853e5e5b647d7e296a0b1cebced27b9e7e
AnikaTahashin10 <121119927+AnikaTahashin10@users.noreply.github.com>
2022-12-22 23:57:18 +0200

    Add files via upload

---
+++ sq_sqrt.c
@@ -0,0 +1,14 @@
+	#include <math.h>
+	#include <stdio.h>
+
+	int main() {
+		float number, squareRoot;
+		int square, num;
+
+		printf("Enter a number : ");
+		scanf_s("%d", &number);
+		squareRoot = sqrt(number);
+			printf("Square root of %d =  %d", number, squareRoot);
+			square = num * num;
+		printf("square of a given number %d is  =  %d", num, square);
+	}
